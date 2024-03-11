708add6a87052e92523de77661801da4e40e3cb2
xzb1bs <beibarssagidolla47@gmail.com>
2024-03-11 18:24:49 +0600

    lab

--- TSIS 6/builtin functions/ex1.py
+++ TSIS 6/builtin functions/ex1.py
@@ -3,10 +3,6 @@ def multiply_list(numbers):
     for num in numbers:
         result *= num
     return result
-    result = 1
-    for num in numbers:
-        result *= num
-    return result
 
 n = int(input())
 list = []
